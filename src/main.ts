14cff263382452332804714165fce4a9b23fcf61
southliu <1275093225@qq.com>
2024-01-23 21:45:19 +0800

    init

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useStaticAssets('static', { prefix: '/pages' });
+  app.enableCors();
+
+  await app.listen(3000);
+}
+bootstrap();
